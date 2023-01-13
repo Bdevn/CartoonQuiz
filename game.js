989275151088505240dee84d3e1022b2b62c2230
Steve B <sbac.freelance@gmail.com>
2023-01-13 14:26:34 -0600

    Update game.js
    commented out location.assign

--- game.js
+++ game.js
@@ -110,5 +110,5 @@ choices.forEach((choice) => {
 incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
-    window.location.assign('https://bdevn.github.io/CartoonQuiz/end.html');
+    // window.location.assign('https://bdevn.github.io/CartoonQuiz/end.html');
 };
