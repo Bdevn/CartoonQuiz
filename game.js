0f77b9414546712207450286565f791befae15e8
Steve B <sbac.freelance@gmail.com>
2023-01-13 18:24:34 -0600

    Update game.js
    edit url

--- game.js
+++ game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('https://bdevn.github.io/end.html');
+        return window.location.assign('https://bdevn.github.io/CartoonQuiz/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
